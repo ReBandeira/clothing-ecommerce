65d31fe319f0ad5b8982fe32554688166bbee3ae
Renata Bandeira <renatabandeiradasilva@gmail.com>
2023-01-06 18:37:22 -0300

    add componentes: lista de categorias, categoria-item

---
+++ src/components/categories-list/categories-list.components.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from '../category-item/category-item.component';
+import './categories-list.styles.sass';
+
+const CategoriesList = ({categories}) => {
+    return (
+        <div className='categories-list-container'>
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category}/>
+            ))}        
+      </div> 
+    )
+}
+export default CategoriesList;
