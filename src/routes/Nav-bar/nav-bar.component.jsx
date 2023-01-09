8261403ad33b42a25136f9ce5b6db553999bfc4f
Renata Bandeira <renatabandeiradasilva@gmail.com>
2023-01-09 08:43:42 -0300

    add rotas

---
+++ src/routes/Nav-bar/nav-bar.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './nav-bar.styles.sass';
+
+const NavBar = () => {
+    return (
+        <Fragment>
+            <div className='nav-bar'>
+                <div>
+                    <Link className='logo-container' to='/'>
+                        <CrownLogo className='logo'/>
+                    </Link>
+                </div>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                            shop
+                    </Link>
+                </div>                
+            </div>
+            <Outlet/>
+        </Fragment>
+       
+    )
+}
+
+export default NavBar;
